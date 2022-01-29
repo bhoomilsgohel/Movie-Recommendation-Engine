ac18dff6cb73ae92b48776b2db30f2857ce44bb9
Bhoomil Gohel <bhoomilsgohel@gmail.com>
2022-01-29 19:20:51 +0530

    machine learning model

--- backend/routes/movies.js
+++ backend/routes/movies.js
@@ -2,9 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 router.route("/").get(function (req, res) {
+    // aslkfdjlasjdfklkjls
     res.status(200).json({
         message: "Welcome to the movies API",
     });
 });
 
 module.exports = router;
+
+// [  ]
