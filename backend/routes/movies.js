6d746e86189046f52f12ce61ca5f3840f3209c1e
neel951 <cse190001044@iiti.ac.in>
2022-01-29 19:09:17 +0530

    Movie Api

--- backend/routes/movies.js
+++ backend/routes/movies.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
-router.route("/").get(function (req, res) {
-    res.status(200).json({
-        message: "Welcome to the movies API",
-    });
-});
+router.route('/').get(function (req, res) {
+  res.status(200).json({
+    message: 'Movie Api',
+  })
+})
 
-module.exports = router;
+module.exports = router
